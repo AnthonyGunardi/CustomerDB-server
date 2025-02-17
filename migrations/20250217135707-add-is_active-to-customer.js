986dbb83459369754ec68be2b9eea0d94b10272a
AnthonyGunardi <anthonygunardi@gmail.com>
2025-02-17 14:09:22 +0700

    add migration file: add is_active field to Customers table

---
+++ migrations/20250217135707-add-is_active-to-customer.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Customers', 'is_active', {
+      type: Sequelize.BOOLEAN,
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
+      })
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Customers', 'is_active')
+  }
+};
